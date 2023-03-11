441fd2638305d3477d89d82932a5220f5e357d97
Oakchris1955 <80592203+Oakchris1955@users.noreply.github.com>
2023-03-11 17:26:30 +0200

    Change in the I2C communication
    Before the commit, in roder to change (or read) and address, you supplied the 4 bits of this address followed by 4 more bits which weren't taken into account. Now, the opposite happens: only the last 4 bits are takne into account

--- main.c
+++ main.c
@@ -21,14 +21,14 @@ static void i2c_slave_handler(i2c_inst_t *i2c, i2c_slave_event_t event) {
 	switch (event) {
 		case I2C_SLAVE_RECEIVE:
 			if(!memory.pwm_reg_addr_written) {
-				memory.pwm_reg_addr = i2c_read_byte_raw(i2c);
+				memory.pwm_reg_addr = i2c_read_byte_raw(i2c) & 0b00001111;
 				memory.pwm_reg_addr_written = true;
 			} else {
-				memory.pwm_registers[memory.pwm_reg_addr >> 4] = i2c_read_byte_raw(i2c);
+				memory.pwm_registers[memory.pwm_reg_addr] = i2c_read_byte_raw(i2c);
 			}
 			break;
 		case I2C_SLAVE_REQUEST:
-			i2c_write_byte_raw(i2c, memory.pwm_registers[memory.pwm_reg_addr >> 4]);
+			i2c_write_byte_raw(i2c, memory.pwm_registers[memory.pwm_reg_addr]);
 			memory.pwm_reg_addr++;
 			break;
 		case I2C_SLAVE_FINISH:
