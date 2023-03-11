d08c54602442ad9c2f5a74e5c06fe2fd1ebefeb7
Oakchris1955 <80592203+Oakchris1955@users.noreply.github.com>
2023-03-11 17:41:27 +0200

    Implement PWM control

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include <hardware/i2c.h>
+#include <hardware/pwm.h>
 #include <pico/i2c_slave.h>
 #include <pico/stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
+#define PWM_BASE_PIN 2
 
 static const uint I2C_SLAVE_ADDRESS = 0x17;
 static const uint I2C_BAUDRATE = 100000; // 100 kHz
@@ -53,11 +55,29 @@ static void setup_slave() {
     i2c_slave_init(i2c0, I2C_SLAVE_ADDRESS, &i2c_slave_handler);
 }
 
+static void setup_pwm() {
+	for (int pin_number = PWM_BASE_PIN; pin_number < PWM_BASE_PIN+16; pin_number++) {
+		gpio_set_function(pin_number, GPIO_FUNC_PWM);
+		pwm_set_gpio_level(pin_number, 0);
+	}
+
+	for (int slice_num = 0; slice_num < 8; slice_num++) {
+		pwm_set_wrap(slice_num, 255);
+		pwm_set_enabled(slice_num, true);
+	}
+}
 
 int main() {
 	stdio_init_all();
 	printf("Initiated stdio_all");
+	setup_pwm();
+	printf("Initiated PWM");
 	setup_slave();
 	printf("Initiated slave");
-	while (1) {};
+	while (1) {
+		for (int reg_address = 0; reg_address < 16; reg_address++) {
+			pwm_set_gpio_level(PWM_BASE_PIN+reg_address, memory.pwm_registers[reg_address]);
+		}
+	};
 }
+
